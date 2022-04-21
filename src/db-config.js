8eb2e8372942c329ac187922aa4618be314fd0af
Francis Brodie <francis.brodie@platform45.com>
2022-04-21 10:40:46 +0200

    added database inserts to table

--- src/db-config.js
+++ src/db-config.js
@@ -1,4 +1,10 @@
 import pg from 'pg';
-let client = new pg.Client({ host: 'localhost', port: 5432, database: 'surfs_app' });
-client.connect(console.error);
+export const client = new pg.Client({ host: 'localhost', port: 5432, database: 'surfs_app' });
+client.connect();
+// let thingToInsert = `Robert ' ); drop table hats; --`;
+// client.query(`INSERT INTO hats (type) VALUES ($1)`, [thingToInsert]).then((res) => {
+//     console.log(res)
+// }).then(() => {
+//     client.end()
+// })
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import { client } from './db-config.js';
 import express from 'express';
+import * as crypto from 'crypto';
+
+let hash = (text) => {
+    let salt = crypto.randomBytes(16).toString('hex')
+    let hash = crypto.pbkdf2Sync(text, salt, 1000, 64, 'sha512')
+    return hash
+}
 let isEmail = (validEmail) => {
     return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail))
 }
@@ -6,12 +14,23 @@ const app = express()
 app.use(express.static('src/public'))
 app.use(express.json())
 app.use(express.urlencoded())
+app.get('/hats', (req, res) => {
+    client.query('SELECT * from hats').then((queryResponse) => {
+        res.json(queryResponse.rows);
+    })
+})
 app.post('/sign-up', (req, res) => {
     if (!isEmail(req.body.email)) {
-        res.status(400).send(); return
+        return res.status(400).send();
     }
-    res.status(201).send()
-    console.log(req.body)
+    const hashed_password = hash(req.body.password)
+    client.query(
+        'INSERT into users (email, language, continent, city, hashed_password) values ($1, $2,  $3, $4, $5)',
+        [req.body.email, req.body.language, req.body.continent, req.body.city, hashed_password]
+    ).then(() => {
+        return res.status(201).send()
+    })
+
 })
 app.listen(3000)
 
